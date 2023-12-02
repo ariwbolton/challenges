6b60e0dfa86bd651cfbec8c0b2302086f13487de
Ari Wolff Bolton <ariwbolton@gmail.com>
2023-12-02 08:31:30 -0800

    ProjectEuler: Prob37

---
+++ ProjectEuler/python3/Prob37.py
@@ -0,0 +1,37 @@
+import sympy
+
+def is_truncatable_prime(n):
+    # Assume prime
+    n_str = str(n)
+    n_digits = len(n_str)
+
+    if not sympy.isprime(n):
+        return False
+
+    # Check all truncations of n
+    for i in range(1, n_digits):
+        left_anchor_truncation = int(n_str[:i])
+        right_anchor_truncation = int(n_str[i:])
+
+        if not sympy.isprime(left_anchor_truncation):
+            return False
+
+        if not sympy.isprime(right_anchor_truncation):
+            return False
+
+    return True
+
+def truncatable_primes():
+    results = []
+
+    i = 11 # Earliest possible truncatable prime; single digit primes not "truncatable"
+
+    while len(results) != 11:
+        if is_truncatable_prime(i):
+            results.append(i)
+
+        i += 1
+
+    return sum(results)
+
+print(truncatable_primes())
