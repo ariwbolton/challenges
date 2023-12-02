ed59393e0e40f4e62c6dbb841405c9ba4c0492de
Ari Wolff Bolton <ariwbolton@gmail.com>
2023-12-02 08:36:53 -0800

    ProjectEuler: Prob36

---
+++ ProjectEuler/python3/Prob36.py
@@ -0,0 +1,7 @@
+def double_base_palindromes(up_to):
+    return sum(i for i in range(up_to + 1) if is_palindrome(str(i)) and is_palindrome("{0:b}".format(i)))
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+print(double_base_palindromes(10 ** 6))
